0b5b0e8a535c8ff1475a5afd7eca296688e09866
Petter5-5 <yeraytrejosanchez@gmail.com>
2026-04-20 19:48:43 +0200

    Ejercicio_03

--- Tema_05/Entrega_01_UD_5/SQLQuery1.sql
+++ Tema_05/Entrega_01_UD_5/SQLQuery1.sql
@@ -21,16 +21,52 @@ EXEC sp_insertarPersona '78598889T', 'Yeray', 'Trejo', 'Sanchez', 'Malaga', 'Av.
 GO
 
 --Ejercicio_03
+GO
+CREATE PROCEDURE sp_asignarProfesor @nombre VARCHAR(25), @apellido1 VARCHAR(50), @apellido2 VARCHAR(50), @departamento VARCHAR(50)
+AS
+BEGIN
+	IF EXISTS(SELECT id FROM persona WHERE nombre = @nombre AND apellido1 = @apellido1 AND apellido2 = @apellido2)
+	BEGIN
+		IF EXISTS(SELECT id FROM departamento WHERE nombre = @departamento)
+		BEGIN
+			INSERT INTO profesor VALUES ((SELECT id FROM persona WHERE nombre = @nombre), (SELECT id FROM departamento WHERE nombre = @departamento));
+			IF((SELECT sexo FROM persona WHERE nombre = @nombre) = 'H')
+			BEGIN
+				PRINT 'El profesor ha sido asignado al departamento correctamente.';
+			END
+			ELSE
+			BEGIN
+				PRINT 'La profesora ha sido asignado al departamento correctamente.';
+			END
+		END
+		ELSE
+		BEGIN
+			;THROW 50015, 'El departamento no existe en la base de datos.', 1;
+		END
+	END 
+	ELSE
+	BEGIN
+		;THROW 50016, 'La persona no existe en la base de datos o no es profesor/a', 1;
+	END
+END;
+GO
 
+EXEC sp_asignarProfesor 'dw', 'aw', 'dwa', 'Derecho';
+GO
+--Ejercicio_04
+CREATE PROCEDURE sp_insertarGrado @nombre
+AS
+BEGIN
+	BEGIN TRY
+		
+	END TRY
+	BEGIN CATCH
+		
+	END CATCH
+END;
+GO
 
-
-
-
-
-
-
-
-
+EXEC sp_insertarGrado 'Grado en Ingeniería Agrícola (Plan 2015)';
 
 
 
